16d5a04b31265a7ed581d5590723a2fcb89b3521
Aida Mirabadi <aida.mirabadi@gmail.com>
2016-07-16 11:18:09 +0430

    Add test for getting commits and email sending

--- src/tarsier/data_services/tests/test_github.py
+++ src/tarsier/data_services/tests/test_github.py
@@ -20,26 +20,26 @@ async def main():
 
     # Define authors list.
     authors = [
-        # Author(
-        #     username='pylover',
-        #     email='vahid.mardani@gmail.com',
-        #     avatar_url='https://avatars.githubusercontent.com/u/1302253?v=3',
-        #     html_url='https://github.com/pylover',
-        #     token='7806436fae6d41b039b9d9320481e1279c29a2e0'
-        # ),
-        # Author(
-        #     username='Sharez',
-        #     email='shahabrezaee@gmail.com',
-        #     avatar_url='https://avatars.githubusercontent.com/u/328063?v=3',
-        #     html_url='https://github.com/Sharez',
-        #     token='7806436fae6d41b039b9d9320481e1279c29a2e0'
-        # ),
+        Author(
+            username='pylover',
+            email='vahid.mardani@gmail.com',
+            avatar_url='https://avatars.githubusercontent.com/u/1302253?v=3',
+            html_url='https://github.com/pylover',
+            token=''
+        ),
+        Author(
+            username='Sharez',
+            email='shahabrezaee@gmail.com',
+            avatar_url='https://avatars.githubusercontent.com/u/328063?v=3',
+            html_url='https://github.com/Sharez',
+            token=''
+        ),
         Author(
             username='aida-mirabadi',
             email='aida.mirabadi@gmail.com',
             avatar_url='https://avatars.githubusercontent.com/u/7857775?v=3',
             html_url='https://api.github.com/users/aida-mirabadi',
-            token='7806436fae6d41b039b9d9320481e1279c29a2e0'
+            token=''
         ),
     ]
 

a85e9e0b77fbedaa8620daeb2371d1e7c9243fed
Aida Mirabadi <aida.mirabadi@gmail.com>
2016-07-16 17:33:35 +0430

    Upgrade config by adding global variables

--- src/tarsier/config/app_cfg.py
+++ src/tarsier/config/app_cfg.py
@@ -1,4 +1,16 @@
+from datetime import datetime
 
-# Base url of data services.
+
+# Github data service configs.
 GITHUB_BASE_URL = 'http://api.github.com'
-DEFAULT_DATE_FORMAT = '%Y-%m-%dT00:00:00'
+EMAIL_SUBJECT = "Daily report - %s " % datetime.today().strftime('%Y-%m-%d')
+
+# Date configs.
+START_DATE_FORMAT = '%Y-%m-%dT00:00:00'
+END_DATE_FORMAT = '%Y-%m-%dT23:59:59'
+
+# SMTP configs.
+HOST = 'smtp.gmail.com'
+PORT = 587
+USERNAME = ''
+PASSWORD = ''

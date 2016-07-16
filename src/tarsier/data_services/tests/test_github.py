fd5a038984dce814b69c5d03068c3a391cfdc960
Aida Mirabadi <aida.mirabadi@gmail.com>
2016-07-16 19:23:31 +0430

    Merge author repositories in an array after async fetch

--- src/tarsier/data_services/tests/test_github.py
+++ src/tarsier/data_services/tests/test_github.py
@@ -12,13 +12,19 @@ async def main():
 
     # Define query params of Github api.
     kwargs = {
-        'since': datetime.today().strftime(app_cfg.START_DATE_FORMAT),
-        'until': (datetime.today() + timedelta(days=1)).strftime(app_cfg.END_DATE_FORMAT)
+        'since': '2016-01-01',
+        # 'until': (datetime.today() + timedelta(days=1)).strftime(app_cfg.END_DATE_FORMAT)
     }
+    # kwargs = {
+    #     'since': datetime.today().strftime(app_cfg.START_DATE_FORMAT),
+    #     'until': (datetime.today() + timedelta(days=1)).strftime(app_cfg.END_DATE_FORMAT)
+    # }
 
     # Define repositories list.
     repositories = [
-        'farakavco/Tarsier', 'farakavco/kakapo', 'farakavco/blueprint', 'farakavco/lutino', 'farakavco/tutorials'
+        'farakavco/Tarsier', 'farakavco/kakapo',
+        'farakavco/blueprint',
+        # 'farakavco/lutino', 'farakavco/tutorials'
     ]
 
     # Define authors list.
@@ -32,7 +38,7 @@ async def main():
         ),
         Author(
             username='Sharez',
-            email='aida.mirabadi@gmail.com',
+            email='shahabrezaee@gmail.com',
             avatar_url='https://avatars.githubusercontent.com/u/328063?v=3',
             html_url='https://github.com/Sharez',
             token=''
@@ -48,18 +54,32 @@ async def main():
 
     commits = await GithubDataService(repositories, authors).get_commits(**kwargs)
 
-    for index, commit in enumerate(commits):
-        daily_report = DailyReport()
-        daily_report.smtp_send(
-            authors[index].email,
-            app_cfg.EMAIL_SUBJECT,
-            {
-                'author': authors[index],
-                'commits': commit,
-                'title': app_cfg.EMAIL_SUBJECT,
-            },
-            template_filename='daily_report.mak'
-        )
+    author_index = 0
+    repo_count = 0
+    reports = []
+
+    for repo_commit in commits:
+        repo_count += 1
+        reports = reports + repo_commit
+
+        if author_index < len(authors) and repo_count == len(repositories):
+
+            daily_report = DailyReport()
+            daily_report.smtp_send(
+                authors[author_index].email,
+                app_cfg.EMAIL_SUBJECT,
+                {
+                    'author': authors[author_index],
+                    'commits': reports,
+                    'commits_flag': True if reports else False,
+                    'title': app_cfg.EMAIL_SUBJECT,
+                },
+                template_filename='daily_report.mak'
+            )
+
+            author_index += 1
+            repo_count = 0
+            reports = []
 
 
 if __name__ == '__main__':

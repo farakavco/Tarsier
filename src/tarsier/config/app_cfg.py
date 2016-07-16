b42c6091affe74b3ad463010c0ff5b5590480fde
Aida Mirabadi <aida.mirabadi@gmail.com>
2016-07-16 11:02:57 +0430

    Add new app config

---
+++ src/tarsier/config/app_cfg.py
@@ -0,0 +1,4 @@
+
+# Base url of data services.
+GITHUB_BASE_URL = 'http://api.github.com'
+DEFAULT_DATE_FORMAT = '%Y-%m-%dT00:00:00'
---
+++ src/tarsier/config/local_cfg.py
@@ -0,0 +1,6 @@
+
+# Config of smtp server.
+HOST = 'smtp.gmail.com'
+PORT = '587'
+USERNAME = ''
+PASSWORD = ''
